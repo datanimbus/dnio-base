20680a6e636043a544f8961dd46b040da653ee55
Jugnu Agrawal <jugnu@appveen.com>
2021-02-10 17:22:07 +0530

    read status api

--- api/controllers/fileTransfers.controller.js
+++ api/controllers/fileTransfers.controller.js
@@ -125,4 +125,28 @@ router.delete('/:id', (req, res) => {
 	});
 });
 
+router.put('/:fileId/readStatus', async (req, res) => {
+	let txnId = req.get(global.txnIdHeader);
+	let fileId = req.params.fileId;
+	// let user = req.headers.user;
+	let isRead = req.body.isRead;
+	try {
+		let doc = await model.findOne({ fileId: fileId });
+		if (!doc) {
+			logger.error(`[${txnId}] File status :: ${fileId} ::  Not found`);
+			return res.status(404).json({ message: 'File not found.' });
+		}
+		logger.debug(`[${txnId}] File status :: ${fileId} :: Found`);
+		logger.debug(`[${txnId}] File status :: ${fileId} :: ${JSON.stringify(doc)}`);
+		doc.isRead = isRead;
+		if (doc._metadata) doc._metadata.lastUpdated = new Date();
+		await doc.save();
+		logger.info(`[${txnId}] File status :: ${fileId} :: Success`);
+		res.json({ message: 'File read status updated successfully.' });
+	} catch (err) {
+		logger.error(`[${txnId}] File status :: ${fileId} :: ${err.message}`);
+		res.status(500).json({ message: err.message });
+	}
+});
+
 module.exports = router;
