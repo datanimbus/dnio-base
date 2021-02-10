21e3bb52a1682a2baa4edd7b9e79610b241e34cb
Manthan Baranwal <manthan.baranwal@appveen.com>
2021-02-10 16:15:53 +0530

    minor fix

--- api/controllers/main.controller.js
+++ api/controllers/main.controller.js
@@ -316,7 +316,7 @@ router.get('/', (req, res) => {
 			}
 			if(specialFields.secureFields && specialFields.secureFields.length && specialFields.secureFields[0]) {
 				let promises = docs.map(e => specialFields.decryptSecureFields(req, e, null));
-				docs = await Promise.all(promises)
+				await Promise.all(promises)
 				promises = null;
 			}
 			res.status(200).json(docs);
@@ -349,7 +349,7 @@ router.get('/:id', (req, res) => {
 				doc = await specialFields.expandDocument(req, doc);
 			}
 			if(specialFields.secureFields && specialFields.secureFields.length && specialFields.secureFields[0]) {
-				doc = await specialFields.decryptSecureFields(req, doc, null);
+				await specialFields.decryptSecureFields(req, doc, null);
 			}
 			res.status(200).json(doc);
 		} catch (e) {
