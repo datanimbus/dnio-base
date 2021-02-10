3505a8dd851ac23e662803e34f9fc099ff9cec21
Jugnu Agrawal <jugnu@appveen.com>
2021-02-10 16:21:46 +0530

    lint fix

--- api/utils/mongoose.utils.js
+++ api/utils/mongoose.utils.js
@@ -52,46 +52,36 @@ function metadataPlugin() {
 
 function generateId(prefix, counterName, suffix, padding, counter) {
 	return async function (next) {
-		try {
-			if (this._id) {
-				return next();
-			}
-			prefix = prefix ? prefix : '';
-			suffix = suffix ? suffix : '';
-			let id = null;
-			if (counter || counter === 0) {
-				const doc = await getCount(counterName);
-				let nextNo = padding ? Math.pow(10, padding) + doc.next : doc.next;
-				nextNo = (nextNo || 0).toString();
-				if (padding && parseInt(nextNo.substr(0, 1)) > 1) {
-					throw new Error('length of _id is exceeding counter');
-				}
-				id = padding ? prefix + nextNo.substr(1) + suffix : prefix + nextNo + suffix;
-			} else if (padding) {
-				id = prefix + rand(padding) + suffix;
-			} else {
-				const doc = await getCount(counterName);
-				id = prefix + doc.next;
-			}
-			this._id = id;
-			next();
-		} catch (e) {
-			throw e;
+		if (this._id) {
+			return next();
 		}
-	}
+		prefix = prefix ? prefix : '';
+		suffix = suffix ? suffix : '';
+		let id = null;
+		if (counter || counter === 0) {
+			const doc = await getCount(counterName);
+			let nextNo = padding ? Math.pow(10, padding) + doc.next : doc.next;
+			nextNo = (nextNo || 0).toString();
+			if (padding && parseInt(nextNo.substr(0, 1)) > 1) {
+				throw new Error('length of _id is exceeding counter');
+			}
+			id = padding ? prefix + nextNo.substr(1) + suffix : prefix + nextNo + suffix;
+		} else if (padding) {
+			id = prefix + rand(padding) + suffix;
+		} else {
+			const doc = await getCount(counterName);
+			id = prefix + doc.next;
+		}
+		this._id = id;
+		next();
+	};
 }
 
 
 async function getCount(counterName) {
-	try {
-		// const collection = configDB.collection('counters');
-		const authorDB = global.authorDB;
-		const collection = authorDB.collection('counters');
-		// return await collection.findOneAndUpdate({ _id: counterName }, { $inc: { next: 1 } }, { upsert: true });
-		return ((await collection.findOneAndUpdate({ _id: counterName }, { $inc: { next: 1 } }, { upsert: true })).value || 0);
-	} catch (e) {
-		throw e;
-	}
+	const authorDB = global.authorDB;
+	const collection = authorDB.collection('counters');
+	return ((await collection.findOneAndUpdate({ _id: counterName }, { $inc: { next: 1 } }, { upsert: true })).value || 0);
 }
 
 
@@ -99,7 +89,7 @@ function rand(_i) {
 	var i = Math.pow(10, _i - 1);
 	var j = Math.pow(10, _i) - 1;
 	return ((Math.floor(Math.random() * (j - i + 1)) + i));
-};
+}
 
 
 module.exports.generateId = generateId;
