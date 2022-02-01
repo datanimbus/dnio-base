4aa499ffd08c16662d7fab410acc6861c40c72cd
kavi <kavi@appveen.com>
2022-02-01 10:42:47 +0530

    logging changes

--- api/controllers/file.controller.js
+++ api/controllers/file.controller.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
-const fs = require('fs');
 const uuid = require('uuid/v1');
-const crypto = require('crypto');
 const log4js = require('log4js');
-const storageEngine = require('@appveen/data.stack-utils').storageEngine;
-const config = require('../../config');
 const mongoose = require('mongoose');
 const moment = require('moment');
+const storageEngine = require('@appveen/data.stack-utils').storageEngine;
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+const config = require('../../config');
 
 const logger = log4js.getLogger(global.loggerName);
 
@@ -14,6 +16,8 @@ router.get('/:id/view', (req, res) => {
 	async function execute() {
 		try {
 			const id = req.params.id;
+			logger.info(`[${req.get(txnid)}] File view request received for id ${id}`);
+
 			let file;
 			try {
 				file = (await global.gfsBucket.find({ filename: id }).toArray())[0];
@@ -47,18 +51,19 @@ router.get('/:id/view', (req, res) => {
 
 router.get('/download/:id', (req, res) => {
 	async function execute() {
-		logger.info('File Download Request received');
-		logger.info('Storage Enigne - ', config.fileStorage.storage);
 		try {
 			const id = req.params.id;
 			const storage = config.fileStorage.storage;
+
+			logger.info(`[${req.get(txnid)}] File download request received for id ${id}`);
+			logger.info(`[${req.get(txnid)}] Storage Enigne - ${storage}`);
 
 			if (storage === 'GRIDFS') {
 				let file;
 				try {
 					file = (await global.gfsBucket.find({ filename: id }).toArray())[0];
 				} catch (e) {
-					logger.error(e);
+					logger.error(`[${req.get(txnid)}] Error finding file - ${e}`);
 					res.status(500).json({ message: e.message });
 				}
 				if (!file) {
@@ -68,7 +73,7 @@ router.get('/download/:id', (req, res) => {
 				res.set('Content-Disposition', 'attachment; filename="' + file.metadata.filename + '"');
 				const readstream = global.gfsBucket.openDownloadStream(file._id);
 				readstream.on('error', function (err) {
-					logger.error(err);
+					logger.error(`[${req.get(txnid)}] Error streaming file - ${err}`);
 					return res.end();
 				});
 				readstream.pipe(res);
@@ -77,14 +82,19 @@ router.get('/download/:id', (req, res) => {
 				try {
 					let file = await mongoose.model('files').findOne({ filename: id });
 
+					logger.debug(`[${req.get(txnid)}] File Found, generating download link.`);
+					logger.trace(`[${req.get(txnid)}] File details - ${JSON.stringify(file)}`);
+
 					let downloadUrl = await storageEngine.azureBlob.downloadFileLink(file, config.fileStorage[storage].connectionString,
 						config.fileStorage[storage].container,
 						config.fileStorage[storage].sharedKey,
 						config.fileStorage[storage].timeout);
 
+					logger.debug(`[${req.get(txnid)}] Redirecting response to Azure download link`);
+
 					res.redirect(downloadUrl);
 				} catch (err) {
-					logger.error(err.message);
+					logger.error(`[${req.get(txnid)}] Error downloading file - ${err.message}`);
 					return res.end();
 				}
 			}
@@ -105,12 +115,14 @@ router.get('/download/:id', (req, res) => {
 
 router.post('/upload', (req, res) => {
 	async function execute() {
-		logger.info('Upload File Request received');
-		logger.info('Storage Enigne - ', config.fileStorage.storage);
 		try {
+			const storage = config.fileStorage.storage;
+
+			logger.info(`[${req.get(txnid)}] File upload request received`);
+			logger.info(`[${req.get(txnid)}] Storage Enigne -  ${config.fileStorage.storage}`);
+
 			const sampleFile = req.file;
 			const filename = sampleFile.originalname;
-			const storage = config.fileStorage.storage;
 
 			if (storage === 'GRIDFS') {
 				fs.createReadStream(sampleFile.path).
@@ -119,32 +131,46 @@ router.post('/upload', (req, res) => {
 						metadata: { filename }
 					})).
 					on('error', function (error) {
-						logger.error(error);
+						logger.error(`[${req.get(txnid)}] Error uploading file - ${error}`);
+
 						res.status(500).json({
-							message: error.message
+							message: `Error uploading File - ${error.message}`
 						});
 					}).
 					on('finish', function (file) {
-						logger.debug('File uploaded to gridFS');
+						logger.info(`[${req.get(txnid)}] File uploaded to GridFS`);
+						logger.trace(`[${req.get(txnid)}] File details - ${JSON.stringify(file)}`);
+
 						res.status(200).json(file);
 					});
 
 			} else if (storage === 'AZURE') {
+				try {
+					let file = await createFileObject(req.file);
 
-				// call function to create file object
-				let file = await createFileObject(req.file);
+					logger.trace(`[${req.get(txnid)}] File object details - ${JSON.stringify(file)}`);
 
-				let pathFile = JSON.parse(JSON.stringify(file));
-				pathFile.path = req.file.path;
-				
-				await storageEngine.azureBlob.uploadFile(pathFile,
-					config.fileStorage[storage].connectionString, config.fileStorage[storage].container);
+					let pathFile = JSON.parse(JSON.stringify(file));
+					pathFile.path = req.file.path;
 
-				let resp = await mongoose.model('files').create(file);
+					await storageEngine.azureBlob.uploadFile(pathFile,
+						config.fileStorage[storage].connectionString, config.fileStorage[storage].container);
 
-				file._id = resp._id;
+					let resp = await mongoose.model('files').create(file);
 
-				res.status(200).json(file);
+					file._id = resp._id;
+					
+					logger.info(`[${req.get(txnid)}] File uploaded to Azure`);
+					logger.trace(`[${req.get(txnid)}] File details - ${JSON.stringify(file)}`);
+
+					res.status(200).json(file);
+				} catch (error) {
+					logger.error(`[${req.get(txnid)}] Error while upploading file - ${error}`);
+
+					res.status(500).json({
+						message: `Error uploading file - ${error.message}`
+					});
+				}
 			}
 		} catch (e) {
 			if (typeof e === 'string') {
@@ -161,6 +187,7 @@ router.post('/upload', (req, res) => {
 	});
 });
 
+// Read a request file object and convert to ds file object format
 async function createFileObject(file) {
 	let fileObj = {};
 	fileObj.length = file.size;
@@ -173,6 +200,7 @@ async function createFileObject(file) {
 	return fileObj;
 }
 
+// Create md5 hash of file data
 async function createMD5Hash(file) {
 	return new Promise((resolve, reject) => {
 		const stream = fs.createReadStream(file.path);
