c051bfd0da2876917175e9ecd41564918be9eea8
Jugnu Agrawal <jugnu@appveen.com>
2022-07-19 18:40:53 +0530

    filter logging

--- codeGen/special-fields-generator.js
+++ codeGen/special-fields-generator.js
@@ -316,6 +316,9 @@ function genrateCode(config) {
 	code.push('async function getDynamicFilter(req, data) {');
 	code.push('\tlet filter;');
 	parseRoleForDynamicFilters(config.role.roles);
+	code.push('\tif(filter)');
+	code.push('\t\tlogger.debug(JSON.stringify(filter));');
+	code.push('\t}');
 	code.push('\treturn filter;');
 	code.push('}');
 	code.push('');
