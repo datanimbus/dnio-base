6c4ac61d6177ad7d09cef0fae6e26cd9f1f6568a
Jugnu Agrawal <jugnu@appveen.com>
2022-08-09 13:12:06 +0530

    ready state

--- api/controllers/health.controller.js
+++ api/controllers/health.controller.js
@@ -8,63 +8,47 @@ const init = require('../../init');
 const logger = log4js.getLogger(global.loggerName);
 let runInit = true;
 
-router.get('/live', (req, res) => {
-	async function execute() {
-		try {
-			logger.trace('Mongo DB State:', mongoose.connection.readyState);
-			logger.trace('NATS State:', client && client.nc ? client.nc.connected : null);
-			if (mongoose.connection.readyState === 1 && client && client.nc && client.nc.connected) {
-				return res.status(200).json();
-			} else {
-				return res.status(400).json();
-			}
-		} catch (e) {
-			if (typeof e === 'string') {
-				throw new Error(e);
-			}
-			throw e;
+router.get('/live', async (req, res) => {
+	try {
+		logger.trace('Mongo DB State:', mongoose.connection.readyState);
+		logger.trace('NATS State:', client && client.nc ? client.nc.connected : null);
+		if (mongoose.connection.readyState === 1 && client && client.nc && client.nc.connected) {
+			return res.status(200).json();
+		} else {
+			return res.status(400).json();
 		}
-	}
-	execute().catch(err => {
+	} catch (err) {
 		logger.error(err);
 		res.status(500).json({
 			message: err.message
 		});
-	});
+	}
 });
 
-router.get('/ready', (req, res) => {
-	async function execute() {
-		try {
-			if (mongoose.connection.readyState != 1) {
-				return res.status(400).end();
-			}
-			logger.trace('Init State:', runInit);
-			if (!runInit) {
-				return res.status(200).json();
-			}
-			try {
-				await init();
-				runInit = false;
-				logger.trace('Setting Init State:', runInit);
-				return res.status(200).json();
-			} catch (e) {
-				logger.error(e);
-				res.status(400).end();
-			}
-		} catch (e) {
-			if (typeof e === 'string') {
-				throw new Error(e);
-			}
-			throw e;
+router.get('/ready', async (req, res) => {
+	try {
+		if (mongoose.connection.readyState != mongoose.ConnectionStates.connected) {
+			return res.status(400).end();
 		}
-	}
-	execute().catch(err => {
+		logger.trace('Init State:', runInit);
+		if (!runInit) {
+			return res.status(200).json();
+		}
+		try {
+			await init();
+			runInit = false;
+			logger.trace('Setting Init State:', runInit);
+			return res.status(200).json();
+		} catch (e) {
+			logger.error(e);
+			res.status(400).end();
+		}
+	} catch (err) {
 		logger.error(err);
 		res.status(500).json({
 			message: err.message
 		});
-	});
+	}
 });
 
 module.exports = router;
