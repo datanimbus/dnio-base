7d6ae6409578f40fd42e52d21cd6b4b57165abee
Jugnu Agrawal <jugnu@appveen.com>
2022-11-16 20:40:05 +0530

    health API

--- api/controllers/health.controller.js
+++ api/controllers/health.controller.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose');
 const log4js = require('log4js');
 
 const client = require('../../queue').client;
+const serviceDoc = require('../../service.json');
 
 const logger = log4js.getLogger(global.loggerName);
 
 
 router.get('/live', async (req, res) => {
 	try {
+		if (serviceDoc && serviceDoc.connectors && serviceDoc.connectors.data && serviceDoc.connectors.data.type == 'MSSQL') {
+			return res.status(200).json();
+		}
 		logger.trace('Mongo DB State:', mongoose.connection.readyState);
 		logger.trace('NATS State:', client && client.nc ? client.nc.connected : null);
 		if (mongoose.connection.readyState == 1 && client && client.nc && client.nc.connected) {
@@ -26,6 +30,9 @@ router.get('/live', async (req, res) => {
 
 router.get('/ready', async (req, res) => {
 	try {
+		if (serviceDoc && serviceDoc.connectors && serviceDoc.connectors.data && serviceDoc.connectors.data.type == 'MSSQL') {
+			return res.status(200).json();
+		}
 		if (mongoose.connection.readyState != 1) {
 			return res.status(400).end();
 		}
