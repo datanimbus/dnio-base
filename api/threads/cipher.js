1706718b42d6671ec12a645c2fee13c3f4eb9c51
kavi-appveen <kavi@appveen.com>
2023-12-20 11:08:54 +0530

    logs

--- api/threads/cipher.js
+++ api/threads/cipher.js
@@ -1,5 +1,6 @@
 const { workerData, parentPort } = require('worker_threads');
 const crypto = require('crypto');
+const log4js = require('log4js');
 
 const IV_LENGTH = 16;
 const action = workerData.action;
@@ -7,6 +8,24 @@ const text = workerData.text;
 const encryptionKey = workerData.encryptionKey;
 const appEncryptionKey = workerData.appEncryptionKey;
 // const SECRET = '34857057658800771270426551038148';
+
+const config = require('../../config');
+
+let additionalLoggerIdentifier = 'Worker/Cipher';
+
+config.appNamespace = process.env.DATA_STACK_APP_NS;
+
+let LOGGER_NAME = config.isK8sEnv() ? `[${config.appNamespace}] [${config.hostname}] [${config.serviceId}] [${additionalLoggerIdentifier}]` : `[${config.serviceId}][${additionalLoggerIdentifier}]`;
+global.loggerName = LOGGER_NAME;
+
+const LOG_LEVEL = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
+global.LOG_LEVEL = LOG_LEVEL;
+
+log4js.configure({
+	appenders: { out: { type: 'stdout', layout: { type: 'basic' } } },
+	categories: { default: { appenders: ['out'], level: LOG_LEVEL } }
+});
+let logger = log4js.getLogger(LOGGER_NAME);
 
 let resultData;
 try {
@@ -22,6 +41,7 @@ try {
 		break;
 	}
 	}
+	logger.trace('Result data in thread :: ', resultData);
 	parentPort.postMessage({ statusCode: 200, body: { data: resultData } });
 } catch (err) {
 	parentPort.postMessage({ statusCode: 500, body: err });
@@ -49,29 +69,39 @@ try {
 // }
 
 function encrypt(plainText, secret) {
-	console.log('Encrypting plain text :: ', plainText);
-	const key = crypto.createHash('sha256').update(secret).digest('base64').substring(0, 32);
-	const iv = crypto.randomBytes(IV_LENGTH);
-	const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+	logger.trace('Encrypting plain text :: ', plainText);
+	logger.trace('Encryption secret :: ', secret);
 	let cipherText;
 	try {
+		const key = crypto.createHash('sha256').update(secret).digest('base64').substring(0, 32);
+		const iv = crypto.randomBytes(IV_LENGTH);
+		logger.trace('Encrypting key and iv :: ', key, iv);
+		const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+		logger.trace('Encryption cipher :: ', cipher);
+
 		cipherText = cipher.update(plainText, 'utf8', 'hex');
 		cipherText += cipher.final('hex');
 		cipherText = iv.toString('hex') + ':' + cipherText;
 	} catch (e) {
+		logger.error('Error encrypting text :: ', e);
 		cipherText = null;
 	}
-	console.log('Encrypted Cipher Text :: ', cipherText)
+	logger.trace('Encrypted Cipher Text :: ', cipherText);
 	return cipherText;
 }
 
 
 function decrypt(cipherText, secret) {
-	const key = crypto.createHash('sha256').update(secret).digest('base64').substring(0, 32);
-	const iv = Buffer.from(cipherText.split(':')[0], 'hex');
-	const textBytes = cipherText.split(':')[1];
-	const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-	let decrypted = decipher.update(textBytes, 'hex', 'utf8');
-	decrypted += decipher.final('utf8');
+	let decrypted;
+	try {
+		const key = crypto.createHash('sha256').update(secret).digest('base64').substring(0, 32);
+		const iv = Buffer.from(cipherText.split(':')[0], 'hex');
+		const textBytes = cipherText.split(':')[1];
+		const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+		decrypted = decipher.update(textBytes, 'hex', 'utf8');
+		decrypted += decipher.final('utf8');
+	} catch (err) {
+		logger.error('Error decrypting text :: ', err);
+	}
 	return decrypted;
 }
